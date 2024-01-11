a86a1da4c5476308743641e19deb42d39a44565d
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2024-01-11 14:23:34 +0700

    Add search bar

--- app/src/main/java/com/example/finalpam_2024/MainActivity.kt
+++ app/src/main/java/com/example/finalpam_2024/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.example.finalpam_2024
 
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.finalpam_2024.navigasi.FilmApp
 import com.example.finalpam_2024.ui.theme.Finalpam_2024Theme
 
@@ -30,18 +52,74 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
+
+
+
+
+
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(modifier: Modifier = Modifier, list: List<String>) {
+    Column(modifier.fillMaxSize()) {
+
+        val textState = remember {
+            mutableStateOf(TextFieldValue(""))
+        }
+
+        SearchView(state = textState , placeHolder = "Search here...", modifier = modifier)
+
+        val searchedText = textState.value.text
+
+        LazyColumn(modifier = Modifier.padding(10.dp)) {
+            items(items = list.filter {
+                it.contains(searchedText)
+            }, key = {it}) {item->
+                ColumnItem(item = item)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Finalpam_2024Theme {
-        Greeting("Android")
+fun ColumnItem(item: String){
+    Column(modifier = Modifier.padding(10.dp)) {
+        Text(text = item, modifier = Modifier.padding(vertical = 10.dp), fontSize = 22.sp)
+        Divider()
+
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchView(
+    state: MutableState<TextFieldValue>,
+    placeholder: String,
+    modifier: Modifier
+) {
+    TextField(
+        value = state.value,
+        onValueChange = {value->
+            state.value = value
+        },
+        modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .clip(RoundedCornerShape(30.dp))
+            .border(2.dp, Color.DarkGray, RoundedCornerShape(30.dp)),
+        placeholder ={
+            Text(text = placeholder)
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color.White
+        ),
+        maxLines = 1,
+        singleLine = true,
+        textStyle = TextStyle(
+            color = Color.Black, fontSize = 20.sp
+        )
+
+    )
 }
