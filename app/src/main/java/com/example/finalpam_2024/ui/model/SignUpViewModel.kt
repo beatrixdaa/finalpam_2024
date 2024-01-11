a2f3dcd873461439c797f2b6adfa65abcfbef290
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2024-01-11 11:52:04 +0700

    Update project

--- app/src/main/java/com/example/finalpam_2024/ui/model/SignUpViewModel.kt
+++ app/src/main/java/com/example/finalpam_2024/ui/model/SignUpViewModel.kt
@@ -1,5 +1,19 @@
 package com.example.finalpam_2024.ui.model
 
-class SignUpViewModel {
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
+
+class SignUpViewModel : ViewModel() {
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    suspend fun signUpUser(email: String, password: String): Boolean {
+        return try {
+            auth.createUserWithEmailAndPassword(email, password).await()
+            true // Sign-up berhasil
+        } catch (e: Exception) {
+            false // Sign-up gagal
+        }
+    }
 
 }
--- app/src/main/java/com/example/finalpam_2024/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/example/finalpam_2024/ui/screen/LoginScreen.kt
@@ -1,2 +1,4 @@
 package com.example.finalpam_2024.ui.screen
 
+import com.example.finalpam_2024.navigasi.DestinasiNavigasi
+
